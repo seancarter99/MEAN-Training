45145ba8e3655ac472e7d1a64f36841c5a050eb3
Erica Hicks <48265629+ericahicks@users.noreply.github.com>
2022-11-16 15:13:42 -0500

    ngStyle and ngClass conditional stylings

--- week2/day3/students-app/src/app/app.module.ts
+++ week2/day3/students-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentListComponent } from './student-list/student-list.component';
 import { AddStudentComponent } from './add-student/add-student.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentListComponent,
-    AddStudentComponent
+    AddStudentComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
